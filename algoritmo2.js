617b646b79f58f530f0f6a3413ce5a6feafa170f
jeanneryssmoreno <morenojeannrys@gmail.com>
2025-10-29 19:07:02 -0400

    nuevo algoritmo

--- algoritmo2.js
+++ algoritmo2.js
@@ -2,7 +2,7 @@
 // semáforos de tu ciudad. Necesitas 
 // una función que gestione cada cambio de
 //  green, a yellow, a red, y luego a green.
-//  Completa la función que toma una cadena como
+//  Completa la función que toma una cadena co
 //  argumento que representa el estado actual de
 //   la luz y devuelve una cadena que representa
 //    el estado al que debe cambiar la luz.
@@ -12,23 +12,9 @@
 let cambioDeLuces = ''
 const controladorSemaforo = (curren) => {
 
-    if (curren === 'green') {
-        return 'yellow'
-    }
-    if (curren === 'yellow') {
-        return 'red'
-    } if (curren === 'red') {
-        return 'green'
-    }
-
-    else {
-
-        return `no coincide`
-
-
-    }
+    return curren === 'yellow' ? 'red' : curren === 'green' ? 'yellow' : undefined
 }
 console.log(cambioDeLuces)
-console.log(controladorSemaforo('green'))
+console.log(controladorSemaforo('yellow'))
 
 

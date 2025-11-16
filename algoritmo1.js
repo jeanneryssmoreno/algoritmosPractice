3d294c450c16aeb1a94ad6acfbcd2e46ed556684
jeanneryssmoreno <morenojeannrys@gmail.com>
2025-11-16 02:03:57 -0400

    algoritmo nuevo

--- algoritmo1.js
+++ algoritmo1.js
@@ -13,3 +13,33 @@ const initialEnd = (str) => {
 }
 
 console.log(initialEnd(word))
+
+
+
+// Esta vez sin historia, sin teoría.
+//  Los ejemplos siguientes te muestran cómo escribir 
+//  la función:accum
+
+// Ejemplos:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+
+//pasos primero hay que convertilo a un array para manipularlo
+// iterar el array y si el indice es mayor a 0 ir incrementano uno y que tenga una secuencia cada vez que tenga una letra
+//luego, hay que poner la primera letra en mayuscula y las demas en miniscula
+//retornar el array unido con el join y el separador
+
+
+const acuum = (str) => {
+
+    return str.split('').map((letters, index) => {
+
+        if (index >= 0) {
+            return letters.repeat(index + 1)
+        }
+    })
+
+}
+console.log(acuum('jsjs'))

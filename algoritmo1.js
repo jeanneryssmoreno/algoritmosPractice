b7effa92b70cc4b204d87ac67aca90c8f45c4e2f
jeanneryssmoreno <morenojeannrys@gmail.com>
2025-11-17 15:56:51 -0400

    nuevoe ejercicio

--- algoritmo1.js
+++ algoritmo1.js
@@ -26,20 +26,43 @@ console.log(initialEnd(word))
 // accum("cwAt") -> "C-Ww-Aaa-Tttt"
 
 
-//pasos primero hay que convertilo a un array para manipularlo
+// pasos primero hay que convertilo a un array para manipularlo
 // iterar el array y si el indice es mayor a 0 ir incrementano uno y que tenga una secuencia cada vez que tenga una letra
-//luego, hay que poner la primera letra en mayuscula y las demas en miniscula
+// luego, hay que poner la primera letra en mayuscula y las demas en miniscula
 //retornar el array unido con el join y el separador
 
 
 const acuum = (str) => {
 
-    return str.split('').map((letters, index) => {
-
-        if (index >= 0) {
-            return letters.repeat(index + 1)
-        }
-    })
+    return str.split('').map((letters, index) => letters.toUpperCase() + letters.toLowerCase().repeat(index)).join('-')
 
 }
-console.log(acuum('jsjs'))
+
+
+
+console.log(acuum('abcd'))
+
+
+// Crea una función que devuelva una matriz de enteros desde n hasta 1 donde n>0.
+// Ejemplo: n=5-->[5,4,3,2,1]
+
+// pasos crear una funcion que reciba enteros
+//poder iterar de orden decendente dependiendo de la condicion
+// podemos utilizar un for para iterar y utilizar el contador
+
+
+const numberDesendent = (number) => {
+
+    let result = [];
+
+    for (let i = number; i >= 1; i--) {
+
+        result.push(i)
+    }
+    return result
+}
+console.log(numberDesendent(5))
+
+
+
+
